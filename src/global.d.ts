6bda36cecaf6d60b9729417501e4e8bdf863bda7
Bernard <cereno.abc@gmail.com>
2025-09-11 15:33:14 +0800

    updated types

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,11 +1,23 @@
 export {};
 
+type GtagCommand = "js" | "config" | "event";
+
+type GtagEventParams = {
+  send_to?: string;
+  value?: number;
+  currency?: string;
+  event_callback?: () => void;
+  [key: string]: unknown; // allow any extra keys
+};
+
+interface Gtag {
+  (command: "js", date: Date): void;
+  (command: "config", targetId: string, config?: Record<string, unknown>): void;
+  (command: "event", action: string, params?: GtagEventParams): void;
+}
+
 declare global {
   interface Window {
-    gtag?: (
-      command: "event",
-      action: string,
-      params: Record<string, string>
-    ) => void;
+    gtag?: Gtag;
   }
 }
